327fd11818c2347f434b2c96443831d2ad0ae7e1
Roberto Mana <roberto.mana@vallauri.edu>
2026-09-21 13:40:05 +0200

    Ese02 aggiunto nuovo film e ripulito lista

--- 01 Richiami di JavaScript/Ese 02 Film/index.js
+++ 01 Richiami di JavaScript/Ese 02 Film/index.js
@@ -1,6 +1,5 @@
 "use strict"
-
-const films = [
+let films = [
     // Id, Title, Favorite, Watch date, Rating (0-5)
     [1, "Pulp Fiction", true, "10-03-2024", 5],
     [2, "21 Grammi", true, "17-03-2024", 3],
@@ -13,22 +12,34 @@ const films = [
 
 let tBody = document.getElementsByTagName("tbody")[0];
 
-for (const film of films) {
-    let row = document.createElement("tr");
-    tBody.appendChild(row);
-    for (let i = 0; i < film.length; i++) {
-        const field = film[i];
-        let cell = document.createElement("td");
-        row.appendChild(cell);
-        if (i == 2) {
-            // è il campo "preferito"
-            createPreferitoInnerHtml(cell, field);
-        } else if (i == 4) {
-            // è il campo "rating"
-            createRatingInnerHtml(cell, field);
-        } else {
-            // altro campo, lo tratto come stringa
-            cell.innerHTML = field;
+addEventListeners()
+visualizza()
+
+function addEventListeners(){
+    let btnAdd = document.getElementById("btn-add")
+    btnAdd.addEventListener("click", addNewFilm)
+    btnClear.addEventListener("click", pulisciLista)
+}
+
+function visualizza(){
+    tBody.innerHTML=""
+    for (const film of films) {
+        let row = document.createElement("tr");
+        tBody.appendChild(row);
+        for (let i = 0; i < film.length; i++) {
+            const field = film[i];
+            let cell = document.createElement("td");
+            row.appendChild(cell);
+            if (i == 2) {
+                // è il campo "preferito"
+                createPreferitoInnerHtml(cell, field);
+            } else if (i == 4) {
+                // è il campo "rating"
+                createRatingInnerHtml(cell, field);
+            } else {
+                // altro campo, lo tratto come stringa
+                cell.innerHTML = field;
+            }
         }
     }
 }
@@ -51,4 +62,34 @@ function createRatingInnerHtml(cell, ratingValue) {
         }
         cell.appendChild(star);
     }
+}
+
+function addNewFilm(){
+    let id = films.length + 1
+    let title = prompt("Inserire il titolo del nuovo film ")
+    let aus = random(0,2)
+    let favorite = Boolean(aus)
+    //console.log(favorite)
+    let today=(new Date()).toLocaleDateString().replaceAll("/", "-")
+    let rating = random(1,6)
+
+    let film =[]
+    film.push(id)
+    film.push(title)
+    film.push(favorite)
+    film.push(today)
+    film.push(rating)
+
+    films.push(film)
+    visualizza()
+}
+
+function pulisciLista(){
+    films=[]
+    visualizza()
+}
+
+
+function random(min, max){
+    return (Math.floor((max-min)*Math.random())) + min
 }
